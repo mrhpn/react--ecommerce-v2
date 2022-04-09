83b4fc2a7c1d325478cae1f7655979e76022f1a5
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-04-09 15:24:44 +0630

    added carousel in home page

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
