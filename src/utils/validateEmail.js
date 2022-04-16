7125ffce279845c79f0030edd01ee912b5aa3766
Htet Phyo Naing <mr.htetphyonaing@gmail.com>
2022-04-16 23:26:18 +0630

    fixed validateEmail for warnings

--- src/utils/validateEmail.js
+++ src/utils/validateEmail.js
@@ -1,4 +1,4 @@
-export const isValidEmail = (email) =>
-  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    email
-  );
+export const isValidEmail = (email) => {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+};
